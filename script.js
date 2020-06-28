e6a46e599136d6e924f2bfb6c20461ddfd2b30dc
Greg Doyle <caligd45@gmail.com>
2020-06-28 12:37:49 -0600

    added alphabet, numbers, and symbols for options as well as upper and loercase options

--- script.js
+++ script.js
@@ -3,6 +3,16 @@ var confirmNumber;
 var confirmCharacter;
 var confirmUppercase;
 var confirmLowercase;
+
+alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+number = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+character = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
+var choices;
+space = [];
+var toUpperCase = function (x) {
+    return x.toUpperCase();
+}
+alpha2 = alpha.map(toUpper)
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
