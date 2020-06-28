a0aabbd5a6b317715d9ce082bcc2dd13d2a590c4
Greg Doyle <caligd45@gmail.com>
2020-06-28 13:22:11 -0600

    added if/else statements to confirm upper/lowercase, numbers and special characters

--- script.js
+++ script.js
@@ -16,6 +16,23 @@ alpha2 = alpha.map(toUpper)
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+get.addEventListener("click", function () {
+    ps = generatePassword();
+    document.getElementById("password").placeholder = ps;
+});
+
+function generatePassword() {
+    enter = parseInt(prompt("Please choose length of password between 8 and 128"));
+    if (!enter) {
+        alert("Must have valid input");
+    } else if (enter < 8 || enter > 128) {
+        enter = parseInt(prompt("You must choose between 8 and 128"));
+    } else {
+        confirmLowercase = confirm("Would you like to use lowercase letters?");
+        confirmUppercase = confirm("Would you like to use uppercase letters?");
+        confirmNumber = confirm("Would you like to use numbers?");
+        confirmCharacter = confirm("Would you like to use special characters?");  
+    };
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
